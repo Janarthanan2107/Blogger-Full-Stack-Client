17f3ec029c9bdf60dd5a4de7f2f20ebbbf3c6890
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-01-08 14:35:01 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import Root from "./root/root.jsx";
-import NotFound from "./NotFound/notfound.Route.jsx";
+import NotFound from "./notFound/notfound.Route.jsx";
 import Home from "./home/home.Route.jsx"
 import CreateBlog from "./create/createBlog.Route.jsx";
 export { Root, Home, CreateBlog, NotFound }
