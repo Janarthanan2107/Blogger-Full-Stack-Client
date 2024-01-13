b3eb36c5a672b3059782c98010f06f6f628969de
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-01-13 20:43:21 +0530

    Update login.Route.jsx

--- src/routes/authentication/login/login.Route.jsx
+++ src/routes/authentication/login/login.Route.jsx
@@ -3,6 +3,7 @@ import { RiQuillPenLine } from "react-icons/ri";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../../../context/user.Context";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const { loginUser, token, loggedInUser } = useUserContext();
@@ -135,12 +136,12 @@ const Login = () => {
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Not a member?
-          <a
-            href="/signUp"
+          <Link
+            to={"/signUp"}
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-2"
           >
             register now
-          </a>
+          </Link>
         </p>
       </div>
     </div>
