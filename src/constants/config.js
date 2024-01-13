fc0cd32947994a2bde4e2ed6c6da5ec4d22014e5
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-01-13 08:49:57 +0530

    Update config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,5 @@
 const config = {
-    // url: "https://important-lion-headscarf.cyclic.app/api/v1/"
-    url: "http://localhost:5000/api/v1/"
+    url: "https://important-lion-headscarf.cyclic.app/api/v1/"
 }
 
 export { config }
